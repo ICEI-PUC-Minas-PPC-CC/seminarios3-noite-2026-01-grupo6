6596134c2c00ca059b6a5d93b46f3a4ae7a16ed5
Rafael Oliveira <r4f4elrflrfl@gmail.com>
2026-03-30 14:46:26 -0300

    Next js, Starter project

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+        <div className={styles.page}>
+            {/* Apenas de exemplo, excluir depois */}
+            <h1 className={styles.title}>Jogo de matemática</h1>
+        </div>
+  );
+}
